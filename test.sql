33bf21047765e92b5e21db3a97de3999f57ba96e
OliviaToth <olivia.toth@slalom.com>
2023-02-07 13:21:14 -0600

    added new test file

---
+++ test.sql
@@ -0,0 +1,5 @@
+-- Big Countries solution in Leetcode
+
+select name, population, area from world where area >= 3000000 
+union
+select name, population, area from world where population >= 25000000 ;
